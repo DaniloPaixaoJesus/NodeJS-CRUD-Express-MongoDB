549675e955ea4b586ef570cbd3c5478a78083d7e
danilopaixao <danilo.oficial@gmail.com>
2015-12-03 16:33:20 -0200

    curso aula 9 nodejs https://www.youtube.com/channel/UCt81zuEqfpsZsaSS8VeVGHw

--- app.js
+++ app.js
@@ -1,60 +1,33 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express = require('express'),
+	routes = require('./routes');
+	user = require('./routes/users');
+	http = require('http');
+	path = require('path');
 
 var app = express();
 
 // view engine setup
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//development only
+if('development' == app.get('env')){
+	app.use(express.errorHandler());
+}	
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', routes.index);
+app.get('/teste', routes.teste);
+app.get('/users', user.list);
+
+app.listen(3000, function() {
+    console.log('Express server listening on port 3000');
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+exports.index = function(req, res) {
+  res.render('index', { title: 'Rota Index' });
+};
 
-module.exports = router;
+exports.teste = function(req, res) {
+  res.render('teste', { title: 'Rota Teste' });
+};
