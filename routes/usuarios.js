ad5335e4822f99d271bde77c7b88e8b17472d15c
danilopaixao <danilo.oficial@gmail.com>
2015-12-05 15:34:58 -0200

    curso aula 16 nodejs https://www.youtube.com/channel/UCt81zuEqfpsZsaSS8VeVGHw

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,11 @@
 module.exports = function(app){
 	var usuarios = app.controllers.usuarios;	
 	app.get('/usuarios', usuarios.index); //action index
-	app.get('/usuarios/create', usuarios.create); //action cadastro
+	app.get('/usuarios/create', usuarios.create); //action create get
+	app.post('/usuarios/create', usuarios.insert); //action create post
 	app.get('/usuarios/list', usuarios.list);
+	app.get('/usuarios/edit/:id', usuarios.edit);
+	app.put('/usuarios/edit/:id', usuarios.update);
+	//app.get('/usuarios/show/:id', usuarios.show);
+	//app.get('/usuarios/delete/:id', usuarios.delete);
 }
