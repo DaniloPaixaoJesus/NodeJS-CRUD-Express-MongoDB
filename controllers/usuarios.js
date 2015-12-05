16baa4bdab744be7c8bc6e71da2dda3ef49c6f70
danilopaixao <danilo.oficial@gmail.com>
2015-12-05 15:34:58 -0200

    curso aula 16 nodejs https://www.youtube.com/channel/UCt81zuEqfpsZsaSS8VeVGHw

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -4,25 +4,63 @@ module.exports = function(app){
 	
 	var UsuarioController = {
 		index: function(req, res){
-			res.render('usuarios/index');
+			Usuario.find(function(err, data){
+				if(err){
+					console.log(err);
+				}
+				res.render('usuarios/index', {listUser: data});
+			});
 		},
 		create: function(req, res){
 			//res.send("respond with a create");
-			var model = new  Usuario({
-				name: 'Danilo fixo user',
-				login: 'danilo',
-				password: '12345'
-			});
+			res.render('usuarios/create');
+		},
+		insert: function(req, res){
+			//res.send("respond with a create");
+			var model = new  Usuario(req.body);
 			model.save(function(err, data){
 				if(err){
 					console.log(err);
 				}else{
-					res.json(data);
+					//res.json(data);
+					res.redirect('/usuarios');
+				}
+			});
+		},
+		edit: function(req, res){
+			//Usuario.findOne({_id: req.params.id});			
+			Usuario.findById(req.params.id, function(err, data){
+				if(err){
+					console.log(err);
+				}else{
+				res.render('usuarios/edit', {user: data});
+				}
+			});
+		},
+		update: function(req, res){
+			//res.render('usuarios/edit');
+			//Usuario.findOne({_id: req.params.id});
+			
+			Usuario.findById(req.params.id, function(err, data){
+				if(err){
+					console.log(err);
+				}else{
+					var model = data;
+					model.name = req.body.name;
+					model.login = req.body.login;
+					model.password = req.body.password;
+					
+					model.save(function(err, data){
+						if(err){
+							console.log(err);
+						}else{
+							res.render('usuarios');
+						}
+					});
 				}
 			});
 		},
 		list: function(req, res){
-			//res.send("respond with a list");
 			Usuario.find(function(err, data){
 				if(err){
 					console.log(err);
